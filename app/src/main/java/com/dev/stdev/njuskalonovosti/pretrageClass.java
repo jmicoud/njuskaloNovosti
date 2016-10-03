3342201865de2df6b6488cf677c6ca779220590c
jmicoud <jmicoud@hotmail.com>
2016-10-03 23:17:41 +0200

    Dorada funkcija i izmjena strukture baze.

---
+++ app/src/main/java/com/dev/stdev/njuskalonovosti/pretrageClass.java
@@ -0,0 +1,39 @@
+package com.dev.stdev.njuskalonovosti;
+
+/**
+ * Created by user on 3.10.2016..
+ */
+
+public class pretrageClass {
+
+    public String getGeneralId() {
+        return generalId;
+    }
+
+    public void setGeneralId(String generalId) {
+        this.generalId = generalId;
+    }
+
+    public String getPretraga() {
+        return pretraga;
+    }
+
+    public void setPretraga(String pretraga) {
+        this.pretraga = pretraga;
+    }
+
+    public String getTip() {
+        return tip;
+    }
+
+    public void setTip(String tip) {
+        this.tip = tip;
+    }
+
+    private String generalId;
+    private String pretraga;
+    private String tip;
+
+
+
+}
