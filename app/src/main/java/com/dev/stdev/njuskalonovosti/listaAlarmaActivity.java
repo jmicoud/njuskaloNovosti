f5dbe5095f8d5c674138c5c13e796bb44f04e684
jmicoud <jmicoud@hotmail.com>
2016-10-04 18:36:26 +0200

    dodana listaalarmaactivity za prikaz svih alarma.

---
+++ app/src/main/java/com/dev/stdev/njuskalonovosti/listaAlarmaActivity.java
@@ -0,0 +1,13 @@
+package com.dev.stdev.njuskalonovosti;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class listaAlarmaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alarma);
+    }
+}
